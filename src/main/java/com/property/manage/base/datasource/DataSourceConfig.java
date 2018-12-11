c7278bcbc97d11bbb0c3067ba32ff34b975f5fdb
guanhuijun <guanghuijun@quicloud.cn>
2018-12-11 11:15:34 +0800

    emoji表情

--- src/main/java/com/property/manage/base/datasource/DataSourceConfig.java
+++ src/main/java/com/property/manage/base/datasource/DataSourceConfig.java
@@ -1,6 +1,7 @@
 package com.property.manage.base.datasource;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.google.common.collect.Lists;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -25,6 +26,7 @@ public class DataSourceConfig {
         dataSource.setUrl(url);
         dataSource.setUsername(username);// 用户名
         dataSource.setPassword(password);// 密码
+        dataSource.setConnectionInitSqls(Lists.newArrayList("set names utf8mb4;"));
         return dataSource;
     }
 
